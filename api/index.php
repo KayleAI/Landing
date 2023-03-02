3d594316c45083b3d13599e4746ece180f3b6338
Arsen Shkrumelyak <storm@nerverift.com>
2023-03-02 20:25:23 +0000

    favicon

--- api/index.php
+++ api/index.php
@@ -1,11 +1,11 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Origin: .kayle.ai");
+header("Access-Control-Allow-Methods: POST");
 // display errors
 //header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
